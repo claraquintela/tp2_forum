a1534df99a420020089f7ec445afbc2af7624be4
Clara Quintela <claraquintela@gmail.com>
2023-11-19 13:37:49 -0500

    Creer le fichier auth.php

--- lib/auth.php
+++ lib/auth.php
@@ -7,15 +7,12 @@ foreach ($_POST as $key => $value) {
     $$key = mysqli_real_escape_string($connex, $value);
 }
 
-// 1- check user
 $sql = "SELECT * FROM user WHERE username = '$username'";
 
 $result = mysqli_query($connex, $sql);
 
-//2 - verifier nombre de lignes
 $count = mysqli_num_rows($result);
 if ($count == 1) {
-    //3 verifier le mot de passe
     $info_user = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $info_user['password'];
     $salt = "H@%h14";
@@ -23,8 +20,6 @@ if ($count == 1) {
 
     if (password_verify($saltPassword, $info_user['password'])) {
 
-
-        //print_r($info_user);
         session_regenerate_id();
         $_SESSION['id'] = $info_user['id'];
         $_SESSION['nom'] = $info_user['nom'];
