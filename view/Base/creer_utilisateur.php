c29697ad6b8c5fd42d1399ac2996d8c39b886a6c
Clara Quintela <claraquintela@gmail.com>
2023-11-19 06:03:26 -0500

    Reorganizer les fichiers dans les dossiers pour une meuilleure structure

---
+++ view/Base/creer_utilisateur.php
@@ -0,0 +1,16 @@
+<form action="">
+    <label> Nom :
+        <input name="nom" type="text">
+    </label>
+    <label> Courriel :
+        <input name="courriel" type="email">
+    </label>
+    <label> Naissance :
+        <input name="date_naissance" type="date">
+    </label>
+    <label> Mot de passe:
+        <input name="motdepasse" type="text">
+    </label>
+
+    <input type="submit" value="Save">
+</form>
